bd4e32fcc570e354d61f1f66e4f35af65b39237a
FonsiWirus <lubiewasa@gmail.com>
2025-04-11 10:34:29 +0200

    Update FonsiOpen.bat

--- FonsiFolder/FonsiOpen.bat
+++ FonsiFolder/FonsiOpen.bat
@@ -3,7 +3,7 @@
 cd /d "%~dp0src"
 
 :: Uruchomienie w pełnym ekranie (cmd bez przycisku zamknięcia)
-start "" /max cmd /c "earth.bat && timeout /t 5 /nobreak && Fonsi.bat && timeout /t 3 /nobreak && FonsiStart.bat && Fonsi2.bat"
+start "" /max cmd /c "earth.bat && timeout /t 5 /nobreak && Fonsi2.bat && Fonsi.bat && timeout /t 3 /nobreak && FonsiStart.bat"
 
 :: Zakończenie skryptu
 exit
