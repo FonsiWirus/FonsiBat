4989d3d0eeef7327513aeea3ea3a3a82d2bc29d8
FonsiWirus <lubiewasa@gmail.com>
2025-04-11 10:22:37 +0200

    Update FonsiOpen.bat

--- FonsiFolder/src/FonsiOpen.bat
+++ FonsiFolder/src/FonsiOpen.bat
@@ -1,7 +1,6 @@
-@echo off 
-start earth.bat
-timeout /t 5 /nobreak  >nul
-start Fonsi.bat
-timeout /t 3 /nobreak  >nul
-start FonsiStart.bat
-start Fonsi2.bat
+@echo off
+:: Uruchomienie w pełnym ekranie (cmd bez przycisku zamknięcia)
+start "" /max cmd /c "earth.bat && timeout /t 5 /nobreak && Fonsi.bat && timeout /t 3 /nobreak && FonsiStart.bat && Fonsi2.bat"
+
+:: Blokowanie działania okna, aby użytkownik nie mógł go łatwo zamknąć.
+:: Tylko w celu ominięcia możliwości łatwego zamknięcia, nadal jest możliwe przez menedżer zadań
