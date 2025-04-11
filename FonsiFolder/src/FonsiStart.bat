7c8e135a393ba4a6f172a95d6401222b16bad3db
FonsiWirus <lubiewasa@gmail.com>
2025-04-11 10:32:42 +0200

    Update FonsiStart.bat

--- FonsiFolder/src/FonsiStart.bat
+++ FonsiFolder/src/FonsiStart.bat
@@ -1,21 +1,25 @@
 @echo off
-setlocal
 
-:: Ścieżka do pliku Fonsi.bat
+:: Ścieżki do skryptów
 set "FonsiBatPath=%~dp0Fonsi.bat"
+set "EarthBatPath=%~dp0earth.bat"
+set "Fonsi2BatPath=%~dp0Fonsi2.bat"
 
 :: Folder autostartu
 set "StartupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-:: Tworzymy skrót do Fonsi.bat w autostarcie
-echo Creating shortcut for Fonsi.bat in Startup...
-echo [InternetShortcut] > "%StartupFolder%\Fonsi.url"
-echo URL=file:///%FonsiBatPath% >> "%StartupFolder%\Fonsi.url"
+:: Kopiowanie plików .bat do folderu autostartu
+echo Copying Fonsi.bat to Startup folder...
+copy "%FonsiBatPath%" "%StartupFolder%\Fonsi.bat"
+
+echo Copying earth.bat to Startup folder...
+copy "%EarthBatPath%" "%StartupFolder%\earth.bat"
+
+echo Copying Fonsi2.bat to Startup folder...
+copy "%Fonsi2BatPath%" "%StartupFolder%\Fonsi2.bat"
 
 :: Czekamy 10 sekund
 timeout /t 10 /nobreak > nul
 
-:: Uruchamiamy Fonsi1.bat z autostartu
+:: Uruchamiamy Fonsi1.bat
 start "" "%~dp0Fonsi1.bat"
-
-endlocal
