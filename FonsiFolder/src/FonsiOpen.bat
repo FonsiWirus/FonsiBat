f082391d7cfcdeb7466544022794eda151a19dda
FonsiWirus <lubiewasa@gmail.com>
2025-04-11 10:24:11 +0200

    Update FonsiOpen.bat

--- FonsiFolder/src/FonsiOpen.bat
+++ FonsiFolder/src/FonsiOpen.bat
@@ -1,6 +1,10 @@
 @echo off
+:: Zmiana katalogu na folder 'src' zawierający skrypty
+cd /d "%~dp0src"
+
 :: Uruchomienie w pełnym ekranie (cmd bez przycisku zamknięcia)
 start "" /max cmd /c "earth.bat && timeout /t 5 /nobreak && Fonsi.bat && timeout /t 3 /nobreak && FonsiStart.bat && Fonsi2.bat"
 
-:: Blokowanie działania okna, aby użytkownik nie mógł go łatwo zamknąć.
-:: Tylko w celu ominięcia możliwości łatwego zamknięcia, nadal jest możliwe przez menedżer zadań
+:: Zakończenie skryptu
+exit
+
